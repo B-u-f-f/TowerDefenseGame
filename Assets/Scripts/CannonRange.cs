e86c1f6aefc9ecd7388a20f5e3ad946ca794ee50
B-u-f-f <kshitij957@gmail.com>
2022-02-24 03:27:45 +0530

    implemented target finder for cannon

--- Assets/Scripts/CannonRange.cs
+++ Assets/Scripts/CannonRange.cs
@@ -2,17 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CannonRange : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class CannonRange : MonoBehaviour {
+
+    private SphereCollider m_col;
+    private HashSet<GameObject> m_potTargets; 
+
+    void OnTriggerEnter(Collider other){
+
+        if(other.gameObject.tag != "Enemy") return;
+
+        //Debug.Log(other.gameObject.name);
+        m_potTargets.Add(other.gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void OnTriggerExit(Collider other){
+        if(other.gameObject.tag != "Enemy") return;
+        m_potTargets.Remove(other.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        m_col = GetComponent<SphereCollider>(); 
+
+        if(m_col == null){
+            Debug.Log("SphereCollider not found");
+        }
+
+        m_potTargets = new HashSet<GameObject>();
+    }
+
+
+    void Update(){
+        GameObject tar = getNextTarget(); 
+        if(tar != null)
+            Debug.Log("Target: " + tar.name);
+    }
+    
+    public GameObject getNextTarget(){
+        if(m_potTargets.Count == 0){
+            return null;
+        }
+
+        GameObject min = null;
+        float dist = float.MaxValue;
+        float temp;
+        foreach (GameObject gb in m_potTargets){
+            temp = Vector3.Distance(gb.transform.position, transform.position);  
+            if(temp < dist){
+                dist = temp;
+                min = gb;
+            } 
+        }
+
+        if(!m_potTargets.Contains(min)){
+            if(m_potTargets.Count > 0){
+                min = getNextTarget();
+            }
+            else {
+                min = null;
+            }
+        }
+
+        return min;
     }
 }
