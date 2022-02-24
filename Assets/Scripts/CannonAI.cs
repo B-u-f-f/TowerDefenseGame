5d9a0f0c2402d104cffaa3e0d70f18fb5cb09ddb
Kaustubh Shrivastava <kaustubh1818@gmail.com>
2022-02-24 21:33:31 +0530

    Enemy Firing in Range

--- Assets/Scripts/CannonAI.cs
+++ Assets/Scripts/CannonAI.cs
@@ -7,7 +7,7 @@ public class CannonAI : MonoBehaviour
 
     private GameObject m_target;
     [SerializeField] private float m_rangeRadius;
-    [SerializeField] private uint m_fireRate;    //milliseconds 
+    [SerializeField] private uint m_fireDelay;    //milliseconds 
     [SerializeField] private uint m_damage;
  
         
@@ -15,17 +15,34 @@ public class CannonAI : MonoBehaviour
     [SerializeField] private Transform m_cannonNozzle;
     [SerializeField] private CannonRange m_cannonRange;
     [SerializeField] private float m_angVelocity;
-    private IEnumerator m_fireCo;
+    private Coroutine m_fireCo;
     //private IEnumerator m_Rotation;
+
+    private int temp_fire;
 
    
     void Start() {
         // set the range on the collider
         m_cannonRange.GetComponent<SphereCollider>().radius = m_rangeRadius;
+        m_fireCo = null;
+        m_angVelocity = 5f;
+        temp_fire = 0;
+        m_fireDelay = 2000;
     }
     
     private IEnumerator cannonFire(){
-        yield return null;
+        while(true){
+            Debug.Log("Fire! Fire! Fire! " + m_target.name + " " + temp_fire);
+        
+            // take away the health of m_target
+            temp_fire += 1;            
+
+            // wait for delay
+            yield return new WaitForSeconds(m_fireDelay / 1000f);
+
+            // shot number
+            
+        }        
     } 
 
     // Update is called once per frame
@@ -34,10 +51,18 @@ public class CannonAI : MonoBehaviour
         
         // if has no target
         if(m_target == null){
-            m_target = m_cannonRange.getNextTarget();  
-            
+            // stop coroutine cannonFire
+            m_target = m_cannonRange.getNextTarget();
+
+            // if enemy dead and m_target = null 
+            // but m_fireCo != null
+            if(m_fireCo != null){
+                StopCoroutine(cannonFire());
+                m_fireCo = null;
+            }
+
             // if no target is available
-            if(m_target == null){
+            if(m_target == null){               
                 return;
             }
         }
@@ -45,11 +70,15 @@ public class CannonAI : MonoBehaviour
         // if target moves out of the range
         if(!m_cannonRange.isTargetInRange(m_target)){
             m_target = null;
+            // stop the coroutine
+            if(m_fireCo != null){
+                StopCoroutine(cannonFire());
+                m_fireCo = null;
+            }
             return;
         }
     
-        // has a target 
-        //
+        // if cannon has a target 
         /// direction to the target 
         Vector3 direction = m_target.transform.position - m_cannonNozzle.position;
         /// angle to rotate to face the target
@@ -59,6 +88,8 @@ public class CannonAI : MonoBehaviour
         /// rotate
         m_cannonNozzle.rotation = Quaternion.Slerp(m_cannonNozzle.rotation, rotation, m_angVelocity * Time.deltaTime);
         
-        // start firing
+        // if cannon not firing at someone else then start firing on m_target
+        if(m_fireCo == null)
+            m_fireCo = StartCoroutine(cannonFire());
     }
 }
