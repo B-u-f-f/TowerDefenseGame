77d0801dc2af9045b8533e758d4130b2031c1d62
abhishek3005-netizen <abhishek.sharma2019@vitstudent.ac.in>
2022-02-24 19:46:13 +0530

    Cannon Rotation Done

--- Assets/Scripts/CannonAI.cs
+++ Assets/Scripts/CannonAI.cs
@@ -19,7 +19,9 @@ public class CannonAI : MonoBehaviour
     [SerializeField] private CannonRange m_cannonRange;
     private IEnumerator m_fireCo;
     private IEnumerator m_Rotation;
-    private bool flag;
+
+    private GameObject pivotPoint;
+    // private bool flag;
     // Start is called before the first frame update
     //
     void Start() {
@@ -29,29 +31,37 @@ public class CannonAI : MonoBehaviour
 
     }
 
-    IEnumerator startRotating(Vector3 pos){
+    // IEnumerator startRotating(Vector3 pos){
     
 
-        while(transform.forward != pos){
-            float angle = Vector3.SignedAngle(transform.forward, pos, Vector3.up);
-            Debug.Log(angle);
+    //     while(transform.forward != pos){
+    //         float angle = Vector3.SignedAngle(transform.forward, pos, Vector3.up);
+    //         Debug.Log(angle);
 
-            target = Quaternion.Euler(0, angle, 0);
+    //         target = Quaternion.Euler(0, angle, 0);
 
-            this.transform.rotation = Quaternion.Slerp(transform.rotation, target, 0.2f);
+    //         this.transform.rotation = Quaternion.Slerp(transform.rotation, target, 0.2f);
 
-            yield return null;
-        }        
+    //         yield return null;
+    //     }        
 
-        flag = true;
-    }
+    //     flag = true;
+    // }
 
     // Update is called once per frame
     void Update() {
         // is target valid
-        Vector3 pos = m_target.transform.position - transform.position;        
-        pos.Normalize();
+        // Vector3 pos = m_target.transform.position - transform.position;        
+        // pos.Normalize();
 
+        // float angle = Vector3.SignedAngle(transform.forward, pos, Vector3.up);
+        // target = Quaternion.Euler(0,angle,0);
+        // this.transform.rotation = Quaternion.Slerp(transform.rotation,target,0.3f);
+
+        Vector3 direction = m_target.transform.position - m_cannonNozzle.position;
+        float angle = Mathf.Atan2(direction.x,direction.z)*Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle,Vector3.up);
+        m_cannonNozzle.rotation = Quaternion.Slerp(m_cannonNozzle.rotation,rotation,5f*Time.deltaTime);
         // rotate nozzle towards the target
         // Vector3 pos = m_target.transform.position - transform.position;
         
@@ -67,15 +77,15 @@ public class CannonAI : MonoBehaviour
         // }
 
 
-        if(pos != transform.forward){
-            if(flag || m_Rotation == null){
-                m_Rotation = startRotating(pos);
-                StartCoroutine(m_Rotation);
-            }  
-            else{
-                // still rotating
-            }          
-        }
+        // if(pos != transform.forward){
+        //     if(flag || m_Rotation == null){
+        //         m_Rotation = startRotating(pos);
+        //         StartCoroutine(m_Rotation);
+        //     }  
+        //     else{
+        //         // still rotating
+        //     }          
+        // }
              
         
 
